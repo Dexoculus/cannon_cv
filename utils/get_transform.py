2c7aa84726aa5349fd3fc245791faf2e719f1688
dexoculus <kirby5122@gmail.com>
2025-05-19 04:17:10 +0900

    update

--- utils/get_transform.py
+++ utils/get_transform.py
@@ -2,10 +2,9 @@ from torchvision import transforms
 
 def get_transform():
     transform = transforms.Compose([
+        transforms.Resize(256),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
     ])
 
     return transform
